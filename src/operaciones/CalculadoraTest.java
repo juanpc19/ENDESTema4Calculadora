53553b1ffe5ff5a0ddd01f2acf8a5f26a2578708
juanpc19 <juan.perez@iesnervion.es>
2023-02-14 10:08:57 +0100

    a

---
+++ src/operaciones/CalculadoraTest.java
@@ -0,0 +1,30 @@
+package operaciones;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class CalculadoraTest {
+
+	@Test
+	void testSuma() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultiplica() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDivide() {
+		fail("Not yet implemented");
+	}
+
+}
